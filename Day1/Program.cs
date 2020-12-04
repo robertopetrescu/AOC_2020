1403bd7f4e2a650d9ed6503940b2c7595fd4ca30
userthis <petrescuroberto@yahoo.com>
2020-12-04 22:44:54 +0200

    Days 1-4

---
+++ Day1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AOC_2020
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Read from file and convert to int
+            string path  = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"input.txt");
+            int[] values = Array.ConvertAll(File.ReadAllLines(path), int.Parse);
+
+            partOne(values);
+            
+            partTwo(values);
+
+        }
+
+        //Sum of 2 numbers = 2020, multiply them
+        public static void partOne(int[] values)
+        {
+            for (int i = 0; i < values.Count(); i++)
+                for (int j = i + 1; j < values.Count(); j++)
+                    if (values[i] + values[j] == 2020)
+                    {
+                        Console.WriteLine(values[i] + " * " + values[j] + " = " + values[i] * values[j]); ;
+                    }
+        }
+
+        //Sum of 3 numbers = 2020, multiply them
+        public static void partTwo(int[] values)
+        {
+            for (int i = 0; i < values.Count(); i++)
+                for (int j = i + 1; j < values.Count(); j++)
+                    for (int k = j + 1; k < values.Count(); k++)
+                        if (values[i] + values[j] + values[k] == 2020)
+                        {
+                            Console.WriteLine(values[i] + " " + values[j] + " " + values[k]);
+                            Console.WriteLine(values[i] * values[j] * values[k]);
+
+                        }
+        }
+    }
+
+
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AOC_2020
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Day 3: Toboggan Trajectory
+
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"input.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            // count the number of '#'
+            partOne(lines);
+
+            // count the number of '#' for multiple input data and multiply the result
+            partTwo(lines);
+            
+        }
+
+        public static void partOne(string[] lines)
+        {
+            int count = 0;
+            
+            int down = 1;
+            int right = 3;
+            int rightIndex = right;
+
+            for (int row = 1; row < lines.Count(); row += down)
+            {
+
+                string line = lines[row];
+                
+                // reset position
+                if (rightIndex >= line.Length)
+                    rightIndex = rightIndex - line.Length;
+
+                Console.WriteLine("{0} is on row {1} and on position {2} contains {3}", line, row, rightIndex, line[rightIndex]);
+
+                if (line[rightIndex].Equals('#'))
+                    count++;
+
+                rightIndex += right;
+            }
+            Console.WriteLine(count);
+        }
+
+        public static void partTwo(string[] lines)
+        {
+            
+            int[] rightArr = { 1, 3, 5, 7, 1 };
+            int[] downArr = { 1, 1, 1, 1, 2 };
+            long nTree = 1;
+            
+
+            for (int i = 0; i < rightArr.Length; i++)
+            {
+
+                int count = 0;
+                int down = downArr[i];
+                int right = rightArr[i];
+                int rightIndex = right;
+
+                for (int row = down; row < lines.Count(); row += down)
+                {
+                    //single line
+                    string line = lines[row];
+
+                    //reset position
+                    if (rightIndex >= line.Length)
+                        rightIndex = rightIndex - line.Length;
+
+                    // count '#'
+                    if (line[rightIndex].Equals('#'))
+                        count++;
+
+                    rightIndex += right;
+                }
+                Console.WriteLine("Value for each: {0}",count);
+                nTree = nTree * count;
+            }
+            Console.WriteLine(nTree);
+
+        }
+
+
+    }
+
+}
+
+
+
